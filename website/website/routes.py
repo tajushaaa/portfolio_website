afae6fd29541756ec3f7c2314c797454ef1fe2d4
taya <115997479+tajushaaa@users.noreply.github.com>
2023-03-28 21:38:56 +0300

    second commit

--- website/website/routes.py
+++ website/website/routes.py
@@ -11,10 +11,12 @@ def home_page():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
-    if form.validate_on_submit():
-        user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+    print("Registering",form.validate_on_submit())
+    if form.validate():
+        print("hi")
+        user = User(name=form.username.data, email=form.email.data, password=form.password.data)
         db.session.add(user)
         db.session.commit()
         flash('Your account has been created! You are now able to log in', 'success')
-        return redirect(url_for('home.html'))
+        return redirect(url_for('home_page'))
     return render_template('register.html', title='Register', form=form)
